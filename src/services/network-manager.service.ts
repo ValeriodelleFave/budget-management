31259b3d3745e5db44b11f19f99960d6605d06ad
ValeriodelleFave <valerio.dellefave00@gmail.com>
2022-12-05 22:44:38 +0100

    Update network-manager.service.ts

--- src/services/network-manager.service.ts
+++ src/services/network-manager.service.ts
@@ -8,14 +8,19 @@ export class NetworkManager {
     headers: new HttpHeaders({ "Content-Type": "application/json" })
   };
 
+  url = {
+    prod: "https://my-endpoints.herokuapp.com",
+    dev: "http://localhost:3000"
+  }
+
   constructor(private http: HttpClient) { }
 
   get<T>(url: Url, options?: { [key: string]: string }) {
-    return this.http.get<T>("http://localhost:3000" + url, { params: options });
+    return this.http.get<T>(this.url.prod + url, { params: options });
   }
 
   post(url: Url, data: Object) {
-    return this.http.post("http://localhost:3000" + url, data);
+    return this.http.post(this.url.prod + url, data);
   }
 
 }
